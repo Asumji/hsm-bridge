82d488fcc1f346e148311fd88f89bc56fcb63e09
Asumji <deralleshaterhi@gmail.com>
2024-04-23 16:35:00 +0200

    e? e!

--- src/events/mineflayer/chat/command.ts
+++ src/events/mineflayer/chat/command.ts
@@ -8,7 +8,6 @@ export default {
 		bot.logger.info(gRank + " " + rank + " " + name + " " + command + " " + spacedArgs)
 
         const args = spacedArgs?.split(" ")
-		console.log(args)
         runCommand(bot,chat,command,name,args)
 	},
 } as Event;
